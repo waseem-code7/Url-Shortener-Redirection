52947cbec09d19e12aae29febc839f5b1fef194e
waseem-code7 <mohammedwaseem2601@gmail.com>
2025-05-29 13:49:32 +0530

    cleaned code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import {Express} from "express";
 import express from "express";
 import DynamoDbClient from "./services/dynamodb"
-
+import RedisService from "./services/redis"
 import routes from "./routers/app.router";
 require('dotenv').config()
 
-
-DynamoDbClient.getInstance()
+const utils = require("./common/utils");
+const logger = utils.getLogger();
 
 const app: Express = express();
 
 
 // middleware
 app.use(express.json())
+
+app.use(function (req, res, next) {
+    logger.info(`${req.method} / ${req.path}`);
+    next();
+})
+
 app.use('/', routes)
 
 function startServer(callback: () => void) {
@@ -20,12 +26,21 @@ function startServer(callback: () => void) {
     const PORT = process.env.PORT || "4003";
 
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}!`);
+        console.log(`Server startup successful, running on port ${PORT}!`);
         callback();
     })
 }
 
-startServer(() => {});
+(async () => {
+    try {
+        DynamoDbClient.getInstance()
+        await RedisService.getInstance()
+        logger.info("DB & cache initialization successful, attempting to start server...")
+        startServer(() => {});
+    } catch (err) {
+        console.error('Error:', err);
+    }
+})();
 
 
 
